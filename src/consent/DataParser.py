368b6222570b0616b091acf147429ef927beef41
Kwyro <kwyroo@proton.me>
2026-07-25 00:21:42 +0400

    Add the DataParser for parse raw data

---
+++ src/consent/DataParser.py
@@ -0,0 +1,18 @@
+import requests
+from requests import Response
+
+def parse(studentID: int) -> dict:
+    """ Парсит данные студента по ID его заявления
+
+    Args:
+        studentID (int): ID заявления
+    Returns:
+        list: массив, в котором указаны все ВУЗы с направленями
+    """
+
+    URL: str = "https://www.postupashkin.ru/priem-2026/forecast?app=" + str(studentID)
+
+    response: Response = requests.get(url=URL)
+    positions = response.json()["positions"]
+
+    return positions
