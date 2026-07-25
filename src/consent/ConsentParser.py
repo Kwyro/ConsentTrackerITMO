fbb9438f202ed023a6982dbdae2bde909bc3812f
Kwyro <kwyroo@proton.me>
2026-07-25 13:58:14 +0400

    Add the function for getting information about consent

--- src/consent/ConsentParser.py
+++ src/consent/ConsentParser.py
@@ -1,3 +1,5 @@
+from typing_extensions import Optional
+
 def processing(programs: list[dict]) -> dict[str, bool]:
     """Обрабатывает сырые данные и получает информацию о имение заявлений в ВУЗЫ
 
@@ -17,4 +19,17 @@ def processing(programs: list[dict]) -> dict[str, bool]:
         universities[name] = consent
 
     return universities
-            
+
+def getConsent(universities: dict[str, bool]) -> Optional[str]:
+    """Возвращает название ВУЗа, куда было согласие, в том случае, если было подано согласие
+
+    Args:
+        universities (dict[str, bool]): хэш-таблица с названиями ВУЗов и имением согласия
+    
+    Returns:
+        Optional[str]: название ВУЗа, где лежит согласие
+    """
+
+    for name, consent in universities.items():
+        if consent: return name
+    return None
